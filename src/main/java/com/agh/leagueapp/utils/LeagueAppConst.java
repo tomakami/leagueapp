12e515014c2675b3d19a1a93735fd1ead2aa0fca
tomakami <58790657+tomakami@users.noreply.github.com>
2022-01-25 14:35:21 +0100

    Hotfix

--- src/main/java/com/agh/leagueapp/utils/LeagueAppConst.java
+++ src/main/java/com/agh/leagueapp/utils/LeagueAppConst.java
@@ -16,11 +16,11 @@ public class LeagueAppConst {
     public static final String PAGE_TEAMS = "teams";
     public static final String PAGE_PLAYERS = "players";
 
-    public static final String API_KEY = "RGAPI-cc199505-0a61-444d-8915-ad59fd5fecf9";
+    public static final String API_KEY = "INSERT API KEY HERE";
 
     public static final boolean USE_STUB = true;
     public static final LeagueShard TEST_SUMMONER_REGION = LeagueShard.EUW1;
-    public static final String TEST_SUMMONER_NAME = "thebausffs";
+    public static final String TEST_SUMMONER_NAME = "testSummonerName";
     public static final int TEST_GAME_COUNT = 20;
 
     public static final List<LeagueShard> VALID_REGIONS = Arrays.asList(
